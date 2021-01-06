88e9c4cb0fe793ca65e2d878898d96cec7e06662
“Vladimir <vladimirsemovskih@gmail.com>
2021-01-06 17:22:56 +0400

    Delete unused files (from september 2020 version)

--- app/src/main/java/com/example/shtormovoy_zaryad/MainActivity.kt
+++ app/src/main/java/com/example/shtormovoy_zaryad/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.example.shtormovoy_zaryad
 
-import android.content.ClipData
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction.*
-import com.example.shtormovoy_zaryad.FragmentsMenu.*
 import com.example.shtormovoy_zaryad.ui.Fragment_Chats_BottomMenu
 import com.example.shtormovoy_zaryad.ui.Fragment_Profile_BottomMenu
 import com.example.shtormovoy_zaryad.ui.Fragment_Stadiums_BottomMenu
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.layout_main_activity.*
 
@@ -74,67 +67,4 @@ class MainActivity : AppCompatActivity() {
     fun setBottomNavigationFragment(fragment: Fragment) {
         fragmentManager.beginTransaction().replace(R.id.frameForFragments, fragment).commit()
     }
-
-
-
-
-
-
-    /*val fr_pole = Fragment_Pole()
-    val fr_chats = Fragment_Chats()
-    val fr_profile = Fragment_Profile()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_layout)
-
-        makeCurrentFragment(cur_fr_chislo)
-
-        bottom_nav_id.setOnNavigationItemSelectedListener {
-            makeCurrentFragment(
-                when (it.itemId) {
-                    R.id.bottom_menu_pole -> pole_pos
-                    R.id.bottom_menu_chats -> chats_pos
-                    R.id.bottom_menu_profile -> profile_pos
-                    else -> no_pos
-                }
-            )
-            true
-        }
-    }
-
-    fun showFragment(fr: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frame_to_replace, fr)
-            setTransition(TRANSIT_FRAGMENT_OPEN)
-            commit()
-        }
-    }
-
-    private fun makeCurrentFragment(chislo: Int) {
-        if (chislo == no_pos) {
-            showFragment(fr_pole)
-            return
-        }
-
-        if (chislo == cur_fr_chislo) return
-
-        cur_fr_chislo = chislo
-
-        when (cur_fr_chislo) {
-            pole_pos -> showFragment(fr_pole)
-            chats_pos -> showFragment(fr_chats)
-            profile_pos -> showFragment(fr_profile)
-        }
-
-    }
-
-    companion object {
-        val no_pos = -1
-        val pole_pos = 0
-        val chats_pos = 1
-        val profile_pos = 2
-
-        var cur_fr_chislo = no_pos
-    }*/
 }
--- app/src/main/java/com/example/shtormovoy_zaryad/RecyclerView_Pole/Data_Pole.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shtormovoy_zaryad.RecyclerView_Pole
-
-
-class Data_Pole() {
-    /*lateinit var name: String
-    var chislo_l: Int = 0
-     var mark: Int = 0
-    lateinit var img_url: String*/
-}
